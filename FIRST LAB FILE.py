e1ff30e7a9f726baff20426f599b340e9e647ec9
asrar <asrar.rifae@hotmail.com>
2023-01-09 14:52:35 +0300

    first python lab

--- FIRST LAB FILE.py
+++ FIRST LAB FILE.py
@@ -1,10 +1,11 @@
 football= ("football team ")
 x= 8
 y= 8.8
-vr1,vr2,vr3 = 1,2,3
 m1=m2=m3 = 233
+vr1,vr2,vr3 = 1,2,3
 
 print(football)
 print(x,y)
-print(vr1,vr2,vr3)
 print(m1,m2,m3)
+print(vr1,vr2,vr3)
+
