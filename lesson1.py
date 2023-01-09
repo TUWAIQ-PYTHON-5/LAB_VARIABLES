d5aa9a935479ff16d9ff37d26f6546011061c3a6
nada.abdullah <none.4292@gmail.com>
2023-01-09 15:02:36 +0300

    my first python lap solved

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+my_fav_football =("alnasser")
+x = 20
+y:float = 20.34
+
+v1= v2= v3 = 50
+
+maha_score , nada_score , mona_score = 99 , 100 , 100
+
+print(x , y)
+print(my_fav_football)
+print(v1,v2,v3)
+print(maha_score , nada_score , mona_score)
