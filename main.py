cd18d89162df9bf1652da779f6954bc560e6ca48
halrasheed <haroon.arrsheed@gmail.com>
2023-01-09 14:49:09 +0300

    Uploading LAB_Variables homework

---
+++ main.py
@@ -0,0 +1,16 @@
+my_team : str = "Wej"
+x : int = 0
+y : float = 0.0
+GX = XG = AV = VA = 3
+A , B , C = 1 , 2 , 3 
+
+print(my_team)
+print(x)
+print(y)
+print(GX)
+print(XG)
+print(AV)
+print(VA)
+print(A)
+print(B)
+print(C)
