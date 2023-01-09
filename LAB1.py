40f5fac0333e283a10252dfe1d90d3668b597314
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-09 14:34:48 +0300

    Assigned the valuables for the correct variables

---
+++ LAB1.py
@@ -0,0 +1,12 @@
+#Define a variable that has a value of your favourite football team.
+footballTeam = "manchester"
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 99
+y = 1
+
+#Assign One Value to Multiple Variables (choose random names & values).
+Ahmad = Khalid = Ali = 21 #years
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+Mohammed, Oday = 171, 180 #cm
