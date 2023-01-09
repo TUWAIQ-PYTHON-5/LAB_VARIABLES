83621bae5666b079977409b7c7e1cdf9b6e2559e
Dooha005 <MINGW64@gmail.com~>
2023-01-09 14:53:02 +0300

    solved LAB_VARIABLES

---
+++ lab1.py
@@ -0,0 +1,22 @@
+# - Define a variable that has a value of your favourite football team.
+My_favourite_football_team : str = ("AL-hilal")
+
+print(My_favourite_football_team)
+
+# - Define a variable “x“  has an integer value and a variable “y” has a float value.
+x : int = 2
+y : int = 2.2
+
+print(x)
+print(y)
+
+#- Assign One Value to Multiple Variables (choose random names & values).
+var1 = var2 = var3 = 1100
+
+print(var1)
+
+# - Assign multiple Values to Multiple Variables (choose random names & values).
+var1, var2 = 35, 40 
+
+print(var1)
+print(var2)
