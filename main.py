adaeed066eb89bd49034c1c1c06d8a30aa90d62f
Yazeed <Yazeedrq9@gmail.com>
2023-01-09 14:48:17 +0300

    task is finished

---
+++ main.py
@@ -0,0 +1,14 @@
+football_team:str = "Real Madrid"
+x:int=5
+y:float=5.5
+
+num1,num2,name1,name2 =25,15, "Yazeed","Mohammed"
+name3 = name4 = name5 = "python"
+
+print(num1)
+print(num2)
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(name5)
