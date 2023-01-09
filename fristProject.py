8d2dde82632f5a66900d283fb5ad8dcbfa024376
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-09 14:53:12 +0300

    add the file

---
+++ fristProject.py
@@ -0,0 +1,13 @@
+fav_football = "Alhilal"
+print (fav_football)
+
+x = 40
+print (x)
+y = 23.5
+print (y)
+
+var1 = var2 = var3 = 25
+var11, var22 = 22, 49
+
+print(var1,var2,var3)
+print(var11,var22)
