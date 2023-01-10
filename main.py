e8905464714f443dfbf4ef7746a352c133f2011e
Instructor <instructor@Aqeels-MacBook-Pro.local>
2023-01-10 10:35:17 +0300

    solved lab 1

---
+++ main.py
@@ -0,0 +1,13 @@
+varble=6
+print(varble)
+my_team= "manchestirunited"
+
+
+print(my_team)
+x=5
+y=7.8
+print(x,y)
+name1=name2=name3=100
+print(name1,name2,name3)
+num1,num2,num3=10,20,30
+print(num1,num2,num3)
