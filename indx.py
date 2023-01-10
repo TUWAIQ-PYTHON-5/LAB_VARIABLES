57f6f971d12dcee17ffca29c51592e018e9a0251
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-10 09:08:50 +0300

    Add files via upload

---
+++ indx.py
@@ -0,0 +1,22 @@
+#1
+print("paris saint")
+
+#2
+x = 5
+y = 67.00
+print(x)
+print(y)
+
+#3
+one = two = three = 654
+print(one) 
+print(two) 
+print(three)
+
+#4
+FaiM, F, FA = 3, 9, 76.0
+print(FaiM)
+print(F)
+print(FA)
+
+
